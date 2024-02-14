817444e37a75fe13100d169967236be69a950262
GalalMohammed <galalmoh2001@gmail.com>
2024-02-14 21:13:53 +0200

    try task 0

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err));
+    this.getAsync = promisify(this.client.get).bind(this.client);
+  }
+
+  isAlive () {
+	  return this.client.connected;
+  }
+
+  async get (key) {
+    return await this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient()
+
+module.exports = redisClient;
