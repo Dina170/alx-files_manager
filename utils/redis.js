108af83c535f47d3cb666eade838b6f41cd37aae
GalalMohammed <galalmoh2001@gmail.com>
2024-02-14 21:16:30 +0200

    fix lint

--- utils/redis.js
+++ utils/redis.js
@@ -2,18 +2,18 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(err));
     this.getAsync = promisify(this.client.get).bind(this.client);
   }
 
-  isAlive () {
-	  return this.client.connected;
+  isAlive() {
+    return this.client.connected;
   }
 
-  async get (key) {
-    return await this.getAsync(key);
+  async get(key) {
+    return this.getAsync(key);
   }
 
   async set(key, value, duration) {
@@ -25,6 +25,6 @@ class RedisClient {
   }
 }
 
-const redisClient = new RedisClient()
+const redisClient = new RedisClient();
 
 module.exports = redisClient;
