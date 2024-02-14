4e86f789ab25680c0e5a57143afc4ed7db6c139f
dina <dinaelhadidy966@gmail.com>
2024-02-14 16:34:09 -0500

    Upload task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,18 @@
+import dbClient from '../utils/db';
+
+const sha1 = require('sha1');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+    const emailFound = dbClient.users.findOne({ email });
+    if (emailFound) return res.status(400).send({ error: 'Already exist' });
+    const hashedPass = sha1(password);
+    const createdUser = dbClient.users.insertOne({ email, password: hashedPass });
+    return res.status(201).send({ id: createdUser._id, email });
+  }
+}
+
+module.exports = UsersController;
