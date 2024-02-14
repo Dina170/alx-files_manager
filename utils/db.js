709c50c1869ad0e62b79ec0aded47d6046580b50
GalalMohammed <galalmoh2001@gmail.com>
2024-02-14 21:48:07 +0200

    add task 1

---
+++ utils/db.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch(console.error);
+  }
+
+  isAlive() {
+    if (this.db) {
+      return true;
+    }
+    return false;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
